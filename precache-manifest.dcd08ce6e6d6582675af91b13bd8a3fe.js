1b6a52a249813b56edf4f229357c4d807d7db9b9
ibrahim cisse <ckader065@gmail.com>
2019-09-26 19:58:28 -0400

    Updates

---
+++ precache-manifest.dcd08ce6e6d6582675af91b13bd8a3fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eecd60a9f80a4bca1626fbdec6bb1a34",
+    "url": "/cookie/index.html"
+  },
+  {
+    "revision": "ee5eb85fa6754e38bb12",
+    "url": "/cookie/static/css/main.e9c5f1a3.chunk.css"
+  },
+  {
+    "revision": "1d32c3e14bc9c41f47d3",
+    "url": "/cookie/static/js/2.3fe41237.chunk.js"
+  },
+  {
+    "revision": "ee5eb85fa6754e38bb12",
+    "url": "/cookie/static/js/main.88b8e6bc.chunk.js"
+  },
+  {
+    "revision": "567da500197fe52c07f6",
+    "url": "/cookie/static/js/runtime-main.a4909ee2.js"
+  }
+]);
